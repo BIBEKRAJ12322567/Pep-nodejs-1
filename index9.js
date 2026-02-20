dea6fc777a6e1fe814ab119618b67e974a9b4a22
{given <{bibekrajtripathy@gmail.com}>
2026-02-20 11:57:45 +0530

    updated

--- index9.js
+++ index9.js
@@ -1,26 +1,49 @@
+// import express from "express";
+// const app=express();
+// app.listen(8080);
+
+// const logger = (req,res,next)=>{
+//     req.message="This is logger function"
+//     //console.log(req.url);
+//     next();
+
+// };
+// app.use(logger)
+
+// app.get("/",(req,res)=>{
+//     //.send("Hello World");
+//     //res.end()
+//     console.log(req.message)
+//     res.json(req.url)
+
+// })
+// app.get("/home",(req,res)=>{
+//     //.send("Hello World");
+//     //res.end()
+//     console.log(req.message)
+//     res.json(req.url)
+
+// })
+
+
+
 import express from "express";
-const app=express();
+const app = express();
 app.listen(8080);
 
-const logger = (req,res,next)=>{
-    req.message="This is logger function"
-    //console.log(req.url);
-    next();
-
+const logger = (req, res, next) => {
+  req.message = "This is logger function";
+  //   console.log(req.url);
+  next();
 };
-app.use(logger)
+// app.use(logger);
+app.get("/", logger, (req, res) => {
+  //   app.use(logger);
+  console.log(req.message);
+  res.json(req.url);
+});
+app.get("/home", (req, res) => {
+  console.log(req.message);
+  res.json(req.url);
+});
 
-app.get("/",(req,res)=>{
-    //.send("Hello World");
-    //res.end()
-    console.log(req.message)
-    res.json(req.url)
-
-})
-app.get("/home",(req,res)=>{
-    //.send("Hello World");
-    //res.end()
-    console.log(req.message)
-    res.json(req.url)
-
-})
